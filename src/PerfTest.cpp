f8e604728c6a382a00190004a12b3c7dd8ef2c85
dopieters <dopieters@gmail.com>
2025-07-12 18:00:35 +0900

    Rework graphics & change mean, var calculation method

--- src/PerfTest.cpp
+++ src/PerfTest.cpp
@@ -1,173 +1,176 @@
 #include "PerfTest.h"
 #include "Utilities.h"
 #include "matplot/matplot.h"
+#include <filesystem>
 
 
 PerfTest::PerfTest(int nCollTest, std::vector<int> NbsVerticesTest):
 	nTest(nCollTest), polNbVerticesTestList(NbsVerticesTest)
 {
+	Initialize();
+}
+
+void PerfTest::Initialize() {
+	//res.AddMethods("Brute force", Geom::PolygonsInterTestBForce);
+	//res.AddMethods("SAT", Geom::PolygonInterTestSAT);
+	//res.AddMethods("SAT Opti", Geom::PolygonInterTestSATOpti);
+	res.AddMethods("SAT Opti iterative", Geom::PolygonInterTestSATOptiItera);
+	res.AddMethods("GJK", Geom::PolygonInterTestGJK);
+
+
+
+	std::filesystem::create_directories("../img");
+
 }
 
 
-template<typename Func>
-void PerfTest::TestMethod(const std::string& funcName, const std::vector<std::pair<Geom::Polygon, Geom::Polygon>>& pols, Func f, float& avgTimeInter, float& avgTimeNoInter)
+void PerfTest::TestMethod(CollisionMethod& method, const std::vector<std::pair<Geom::Polygon, Geom::Polygon>>& pols)
 {
 	if (pols.empty()) return;
 
-	int nInter = 0;
-	avgTimeInter = 0.f;
-	avgTimeNoInter = 0.f;
+	std::vector<double> interTime;
+	std::vector<double> noInterTime;
+
 	for (const auto& pairPol : pols) {
-		const auto resBForces = measureExecutionTime(f, pairPol.first,
-			pairPol.second);
+		const auto resBForces = measureExecutionTime(method.fcn, pairPol.first, pairPol.second);
 
 		if (resBForces.first) {
-			avgTimeInter += resBForces.second;
-			++nInter;
+			interTime.push_back(resBForces.second);
 		}
 		else {
-			avgTimeNoInter += resBForces.second;
+			noInterTime.push_back(resBForces.second);
 		}
 	}
-	avgTimeInter = nInter == 0 ? -1 : avgTimeInter / nInter;
-	avgTimeNoInter = pols.size() - nInter == 0 ? -1 : avgTimeNoInter / nInter;
-	//PrintResult(funcName, pols[0].first.vertices.size(), avgTimeInter, avgTimeNoInter, nInter);
+
+	std::cout << "vertices " << pols[0].first.vertices.size() << "\n";
+	std::cout << method.name << " " << interTime.size() << " pols intersection \n";
+	std::cout << method.name << " " << noInterTime.size() << " pols not intersecting \n";
+
+
+	auto CalcWelford = [](const std::vector<double>& data) -> std::pair<double, double> {
+		double mean = 0.0;
+		double M2 = 0.0;
+		int n = 0;
+		for (double x : data) {
+			++n;
+			double delta = x - mean;
+			mean += delta / n;
+			double delta2 = x - mean;
+			M2 += delta * delta2;
+		}
+		if (n < 2) return { mean, 0.0 };
+		return { mean, M2 / (n - 1) };
+		};
+
+
+	auto [avgInter, varInter] = CalcWelford(interTime);
+	auto [avgNoInter, varNoInter] = CalcWelford(noInterTime);
+
+	auto CalcAverage = [](std::vector<double> measures) -> double
+		{
+			const int size = measures.size();
+			assert(size > 0);
+			double avg = 0.f;
+			for (auto time : measures) {
+
+				avg += time;
+			}
+
+			avg /= size;
+			return avg;
+		};
+
+
+	method.interRes.mu.push_back(avgInter);
+	method.noInterRes.mu.push_back(avgNoInter);
+
+	method.interRes.sigma.push_back(varInter);
+	method.noInterRes.sigma.push_back(varNoInter);
+
+}
+
+void PerfTest::DrawResults(std::vector<std::string> methodNames)
+{
+
+	std::vector<std::string> markers = { "o", "s", "d", "^", "v", "*", "x" };
+	std::vector<std::string> colors = { "blue", "red", "green", "magenta", "cyan", "black", "orange" };
+
+
+	std::string methodName = "../img/";
+	for (const auto& name : methodNames) {
+		methodName += name;
+	}
+
+	
+
+	auto DrawResult = [&](const bool isCollision) {
+		//Create new fig
+		auto figure = matplot::figure(true);
+		matplot::hold(true);
+		figure->size(1200, 800);
+		for (const auto& name : methodNames) {
+			int methodIndex = res.GetMethodIndexByName(name);
+			if (methodIndex < 0) continue;
+
+			const auto methRes = res.GetMethodResultsByIndex(methodIndex, isCollision);
+			auto h = matplot::errorbar(polNbVerticesTestList, methRes.mu, methRes.sigma);
+			h->display_name(name);
+
+			//h->marker(markers[methodIndex % markers.size()]);
+			h->color(colors[methodIndex % colors.size()]);
+			std::cout << "Ploting method :" << name << std::endl;
+
+		}
+
+
+		matplot::legend()->location(matplot::legend::general_alignment::bottomright); // Show legend only if something was plotted
+
+		const std::string title = isCollision ? "Colliding Polygons" : "Non Colliding Polygons";
+		matplot::title(title);
+		matplot::ylabel("Average time (ms)");
+		matplot::xlabel("Polygon vertices");
+
+		std::string img = methodName + title + ".png";
+		img.erase(std::remove(img.begin(), img.end(), ' '), img.end());
+		matplot::save(img);
+		matplot::show();
+		
+		};
+
+
+
+	DrawResult(true);
+	DrawResult(false);
+
+	
+	
 }
 
 void PerfTest::Run()
 {
+	std::vector<std::pair<Geom::Polygon, Geom::Polygon>> pols;
+	pols.reserve(polNbVerticesTestList.size());
 
-	std::vector<float> BForceResInter(polNbVerticesTestList.size()), BForceResNoInter(polNbVerticesTestList.size());
-	std::vector<float> BSATInter(polNbVerticesTestList.size()), BSATNoInter(polNbVerticesTestList.size());
-	std::vector<float> BGJKInter(polNbVerticesTestList.size()), BGJKNoInter(polNbVerticesTestList.size());
-	std::vector<float> BSATRedPolInter(polNbVerticesTestList.size()), BSATRedPolNoInter(polNbVerticesTestList.size());
-	std::vector<float> BSATRedPolIterativeInter(polNbVerticesTestList.size()), BSATRedPolIterativeNoInter(polNbVerticesTestList.size());
+	for (const auto nVert : polNbVerticesTestList) {
+		pols.clear();
 
-
-	std::vector<float> logX;
-	{
-		std::vector<std::pair<Geom::Polygon, Geom::Polygon>> pols;
-		pols.reserve(polNbVerticesTestList.size());
+		// gen polygons
+		for (int ii = 0; ii < nTest; ++ii) {
+			pols.push_back({ Geom::MakeConvexPol(nVert), Geom::MakeConvexPol(nVert) });
+		}
 
 
-		
-
-		int i = 0;
-		for (const auto nVert : polNbVerticesTestList) {
-			logX.push_back(std::log10(nVert));
-			pols.clear();
-
-			// gen polygons
-			for (int ii = 0; ii < nTest; ++ii) {
-				pols.push_back({ Geom::MakeConvexPol(nVert), Geom::MakeConvexPol(nVert) });
-			}
-
-			// measure times
-			//TestMethod("Brute force", pols, Geom::PolygonsInterTestBForce, BForceResInter[i], BForceResNoInter[i]);
-			//TestMethod("SAT", pols, Geom::PolygonInterTestSAT, BSATInter[i], BSATNoInter[i]);
-			//TestMethod("SAT Opti", pols, Geom::PolygonInterTestSATOpti, BSATRedPolInter[i], BSATRedPolNoInter[i]);
-			TestMethod("SAT Opti iterative", pols, Geom::PolygonInterTestSATOptiItera, BSATRedPolIterativeInter[i], BSATRedPolIterativeNoInter[i]);
-			TestMethod("GJK", pols, Geom::PolygonInterTestGJK, BGJKInter[i], BGJKNoInter[i]);
-
-			++i;
-
+		for (auto& method: res.methods) {
+			TestMethod(method, pols);
 		}
 	}
 
 
-	auto DrawResult = [&](const std::vector<std::vector<float>>& Yaxis, std::string title, const std::vector<std::string>& legend, float YaxisMax = -1) {
-		//Create new fig
-		auto figure = matplot::figure(true);
-		figure->size(1200, 800);
-		if (YaxisMax> 0) matplot::ylim({ 0, YaxisMax });
-		matplot::title(title);
-		matplot::ylabel("Time (ms)");
-		matplot::xlabel("N_{vertices}");
-		matplot::plot(polNbVerticesTestList, Yaxis);
-		matplot::xticks({ 0, 100, 200, 300, 400, 500 });
-		auto l = ::matplot::legend(legend);
-
-		l->location(matplot::legend::general_alignment::topleft);
-		l->font_size(10);
-		matplot::save(title +".png");
-		matplot::show();
-
-		};
-	//
-	//// Draw results when there is intersection
-	//{
-	//	std::vector<std::vector<float>> YInter;
-	//	std::vector<std::string> legend;
-	//	//YInter.push_back(BForceResInter); legend.push_back("Brute force");
-	//	YInter.push_back(BSATInter);legend.push_back("SAT");
-	//	YInter.push_back(BSATRedPolInter); legend.push_back("SAT Opti");
-	//	YInter.push_back(BSATRedPolIterativeInter); legend.push_back("Iterative");		
-	//	YInter.push_back(BGJKInter); legend.push_back("GJK");
-	//	DrawResult(YInter, "Polygon intersecting", legend);
-	//		//std::max(BSATRedPolIterativeInter[polNbVerticesTestList.size() - 1], BGJKInter[polNbVerticesTestList.size() - 1]));
-
-	//}
-
-	//// Draw zoom version on small Y when there is no intersection
-	//{
-	//	std::vector<std::vector<float>> YInter;
-	//	std::vector<std::string> legend;
-	//	YInter.push_back(std::vector<float>(BSATInter.begin(), BSATInter.begin() + 5));legend.push_back("SAT");
-	//	YInter.push_back(std::vector<float>(BSATRedPolInter.begin(), BSATRedPolInter.begin() + 5)); legend.push_back("SAT Opti");
-	//	YInter.push_back(std::vector<float>(BSATRedPolIterativeInter.begin(), BSATRedPolIterativeInter.begin() + 5)); legend.push_back("Iterative");
-	//	YInter.push_back(std::vector<float>(BGJKInter.begin(), BGJKInter.begin() + 5)); legend.push_back("GJK");
-
-	//	DrawResult(YInter, "Polygon intersecting zoomed", legend);
-	//}
-
-
-	// Draw results when there is intersection with only GJK and SAT opti
-	{
-		std::vector<std::vector<float>> YInter;
-		std::vector<std::string> legend;
-		YInter.push_back(BSATRedPolIterativeInter); legend.push_back("Iterative");
-		YInter.push_back(BGJKInter); legend.push_back("GJK");
-		DrawResult(YInter, "Polygon Intersecting", legend);
-	}
-
-
-	//// Draw results when there is no intersection
-	//{
-	//	std::vector<std::vector<float>> YNoInter;
-	//	std::vector<std::string> legend;
-	//	//YNoInter.push_back(BForceResNoInter); legend.push_back("Brute force");
-	//	YNoInter.push_back(BSATInter);legend.push_back("SAT");
-	//	YNoInter.push_back(BSATRedPolNoInter); legend.push_back("SAT Opti");
-	//	YNoInter.push_back(BSATRedPolIterativeNoInter); legend.push_back("Iterative");
-	//	YNoInter.push_back(BGJKNoInter); legend.push_back("GJK");
-
-	//	DrawResult(YNoInter, "Polygon not intersecting", legend);
-	//}
-
-	//// Draw zoom version on small Y when there is no intersection
-	//{
-	//	std::vector<std::vector<float>> YNoInter;
-	//	std::vector<std::string> legend;
-	//	YNoInter.push_back(std::vector<float>(BSATNoInter.begin(), BSATNoInter.begin() + 5));legend.push_back("SAT");
-	//	YNoInter.push_back(std::vector<float>(BSATRedPolNoInter.begin(), BSATRedPolNoInter.begin() + 5)); legend.push_back("SAT Opti");
-	//	YNoInter.push_back(std::vector<float>(BSATRedPolIterativeNoInter.begin(), BSATRedPolIterativeNoInter.begin() + 5)); legend.push_back("Iterative");
-	//	YNoInter.push_back(std::vector<float>(BGJKNoInter.begin(), BGJKNoInter.begin() + 5)); legend.push_back("GJK");
-
-	//	DrawResult(YNoInter, "Polygon not intersecting zoomed", legend);
-	//}
-
-
-	// Draw results when there is no intersection only sat opti gjk and no inter
-	{
-		std::vector<std::vector<float>> YNoInter;
-		std::vector<std::string> legend;
-		YNoInter.push_back(BSATRedPolIterativeNoInter); legend.push_back("Iterative");
-		YNoInter.push_back(BGJKNoInter); legend.push_back("GJK");
-		DrawResult(YNoInter, "Polygon not Intersecting", legend);
-	}
-
+	DrawResults({ "SAT Opti iterative", "GJK" });
 
 }
+
+
 
 void PerfTest::PrintResult(const std::string& funcName, int polVert, float timeInter, float timeNoInter, int nInter) const
 {
--- src/PerfTest.h
+++ src/PerfTest.h
@@ -2,12 +2,51 @@
 #include "Geom.h"
 #include <vector>
 #include <string>
+#include <cassert>
+#include"functional"
 
 
-struct TestResults
-{
-	float avgTimeInter = 0.f;
-	float avgTimeNoInter = 0.f;
+struct Results {
+	std::vector<double> mu;
+	std::vector<double> sigma;
+};
+
+struct CollisionMethod {
+	std::string name;
+	std::function<bool(const Geom::Polygon& RESTRICT A, const Geom::Polygon& RESTRICT B) >  fcn;
+
+	Results interRes;
+	Results noInterRes;
+};
+
+
+struct PerformanceResults {
+	std::vector<CollisionMethod> methods;
+	
+	void AddMethods(const std::string& name, std::function<bool(const Geom::Polygon& RESTRICT A, const Geom::Polygon& RESTRICT B) > fcn) {
+		Results emptyRes; emptyRes.mu.clear(); emptyRes.sigma.clear();
+		methods.emplace_back(CollisionMethod{ name, fcn, emptyRes, emptyRes});
+	}
+
+
+	int GetMethodIndexByName(const std::string& name) {
+		int retIndex = -1;
+		for (int ind = 0; ind < methods.size(); ++ind) {
+			if (methods[ind].name == name) {
+				retIndex = ind;
+				break;
+			}
+		}
+
+		return retIndex;
+	}
+
+	Results& GetMethodResultsByIndex(int ind, bool isIntersection) {
+		// Make sure the mindex exist
+		assert(ind >= 0 && ind < methods.size());
+
+		return isIntersection ? methods[ind].interRes: methods[ind].noInterRes;
+	}
 
 };
 
@@ -22,11 +61,12 @@ public:
 private:
 	const int nTest;
 	std::vector<int> polNbVerticesTestList;
+	PerformanceResults res;
 
-
+	void Initialize();
 	void PrintResult(const std::string& funcName, int polVert, float timeInter, float timeNoInter, int nInter) const;
-	template <typename Func>
-	void TestMethod(const std::string& funcName, const std::vector<std::pair<Geom::Polygon, Geom::Polygon>>& pairPol, Func f, float& avgTimeInter, float& avgTimeNoInter);
+	void TestMethod(CollisionMethod& method, const std::vector<std::pair<Geom::Polygon, Geom::Polygon>>& pols);
+	void DrawResults(std::vector<std::string> methodNames);
 };
 
 
